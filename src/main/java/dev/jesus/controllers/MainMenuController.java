98c16a485206b77c641feffead07da785af873f0
Jesus Martin <jesus8.mb@gmail.com>
2025-08-13 19:35:52 +0200

    feat: añadido clase view

---
+++ src/main/java/dev/jesus/controllers/MainMenuController.java
@@ -0,0 +1,11 @@
+package dev.jesus.controllers;
+
+public class MainMenuController {
+
+  public static void mainMenuController(int option) {
+
+    if (option == 1)
+      AddMomentView.printAddMoment();
+  }
+
+}
---
+++ src/main/java/dev/jesus/controllers/views/View.java
@@ -0,0 +1,9 @@
+package dev.jesus.controllers.views;
+
+import java.util.Scanner;
+
+public abstract class View {
+
+  protected static Scanner SCANNER = new Scanner(System.in);
+
+}
