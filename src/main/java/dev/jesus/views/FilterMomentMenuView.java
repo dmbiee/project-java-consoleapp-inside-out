eea4ff6cf329479d9bcbb59ca3b6704e8257b1ae
Jesus Martin <jesus8.mb@gmail.com>
2025-08-20 14:20:35 +0200

    refactorized printfiltermenu

--- src/main/java/dev/jesus/views/FilterMomentMenuView.java
+++ src/main/java/dev/jesus/views/FilterMomentMenuView.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import dev.jesus.controllers.FilterMenuController;
-import dev.jesus.models.EmotionEnum;
 
 public class FilterMomentMenuView extends View {
   public static void printFilterMenu() {
